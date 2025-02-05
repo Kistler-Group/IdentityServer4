5f41b8aa37ba08eb4d4fd579f22a6310173a6cb9
Dano V <daniel.vaclavik@kistler.com>
2025-02-05 09:23:57 +0100

    revert test ISystemClock -> TimeProvider

--- src/IdentityServer4/src/Validation/Default/TokenRequestValidator.cs
+++ src/IdentityServer4/src/Validation/Default/TokenRequestValidator.cs
@@ -35,6 +35,7 @@ namespace IdentityServer4.Validation
         private readonly IResourceOwnerPasswordValidator _resourceOwnerValidator;
         private readonly IProfileService _profile;
         private readonly IDeviceCodeValidator _deviceCodeValidator;
+        private readonly ISystemClock _clock;
         private readonly ILogger _logger;
 
         private ValidatedTokenRequest _validatedRequest;
@@ -54,6 +55,7 @@ namespace IdentityServer4.Validation
         /// <param name="tokenValidator">The token validator.</param>
         /// <param name="refreshTokenService"></param>
         /// <param name="events">The events.</param>
+        /// <param name="clock">The clock.</param>
         /// <param name="logger">The logger.</param>
         public TokenRequestValidator(IdentityServerOptions options, 
             IAuthorizationCodeStore authorizationCodeStore, 
@@ -66,11 +68,13 @@ namespace IdentityServer4.Validation
             IResourceStore resourceStore,
             ITokenValidator tokenValidator, 
             IRefreshTokenService refreshTokenService,
-            IEventService events,  
+            IEventService events, 
+            ISystemClock clock, 
             ILogger<TokenRequestValidator> logger)
         {
             _logger = logger;
             _options = options;
+            _clock = clock;
             _authorizationCodeStore = authorizationCodeStore;
             _resourceOwnerValidator = resourceOwnerValidator;
             _profile = profile;
@@ -245,7 +249,7 @@ namespace IdentityServer4.Validation
             // todo: set to consumed in the future?
             await _authorizationCodeStore.RemoveAuthorizationCodeAsync(code);
 
-            if (authZcode.CreationTime.HasExceeded(authZcode.Lifetime, TimeProvider.GetUtcNow().UtcDateTime))
+            if (authZcode.CreationTime.HasExceeded(authZcode.Lifetime, _clock.UtcNow.UtcDateTime))
             {
                 LogError("Authorization code expired", new { code });
                 return Invalid(OidcConstants.TokenErrors.InvalidGrant);
@@ -262,7 +266,7 @@ namespace IdentityServer4.Validation
             /////////////////////////////////////////////
             // validate code expiration
             /////////////////////////////////////////////
-            if (authZcode.CreationTime.HasExceeded(_validatedRequest.Client.AuthorizationCodeLifetime, TimeProvider.GetUtcNow().UtcDateTime))
+            if (authZcode.CreationTime.HasExceeded(_validatedRequest.Client.AuthorizationCodeLifetime, _clock.UtcNow.UtcDateTime))
             {
                 LogError("Authorization code is expired");
                 return Invalid(OidcConstants.TokenErrors.InvalidGrant);
